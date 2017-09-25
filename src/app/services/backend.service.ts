91b6c87a4ef0f7ca29931dd86e43958423922373
Florian Golemo <fgolemo@gmail.com>
2017-09-25 04:06:33 -0400

    added basic facade toggling

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -2,22 +2,26 @@ import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/toPromise';
-import { environment } from '../../environments/environment';
+import {environment} from '../../environments/environment';
 
 
 @Injectable()
 export class BackendService {
   logged_in = false;
   token = '';
+  facadeRunning = false;
+  knowFacadeState = 0;
 
-  constructor(private http: Http) {  };
+  constructor(private http: Http) {
+    // TODO: get facade state, then set the knownstatevar and finally change template to show loading bar until this state is done
+  };
 
   public login(user: string, pass: string): Promise<any> {
     // TODO: construct proper JWT payload here
     const payload = new URLSearchParams();
     payload.append('username', user);
     payload.append('password', pass);
-    // TODO: there is something wrong here - this doesn't get sent properly
+    // TODO: there is something wrong here - this doesn't get sent properly, fix protocol/form-tokenization
     const loginOp = this.http.post(environment.backend + 'login', payload.toString()).toPromise();
     return loginOp.then(data => this.loginResponse(data), err => this.handleError(err));
   }
@@ -31,6 +35,15 @@ export class BackendService {
       console.log('logged in');
     }
     return Promise.resolve({});
+  }
+
+  public switchFacade(): boolean {
+    console.log(this.facadeRunning ? 'was running' : 'was stopped');
+    console.log('switching facade on/off');
+    // TODO: add actual call here
+    this.facadeRunning = !this.facadeRunning;
+    console.log(this.facadeRunning ? 'is running now' : 'is stopped now');
+    return true;
   }
 
   // public isOn(): Promise<any> {
@@ -56,8 +69,6 @@ export class BackendService {
     console.error(errMsg);
     return Observable.throw(errMsg);
   }
-
-
 
 
   // getNearbyBarsCSV(lat: number, long: number, dist: number, search: string): Promise<any> {
